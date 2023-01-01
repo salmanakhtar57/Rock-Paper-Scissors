ac516d5b501c4089b4288c21f9cc47e3ed3ee3af
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-01-01 20:34:11 +0500

    working on getting random number from the computer.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Let's play a game!")
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices(randomNumber);
+}
+
+console.log(getComputerChoice());
+
+// function randomSelection() {
+//     const randomIndex = Math.random * SELECTIONS.length;
+//     return SELECTIONS(randomIndex)
+// }
+
+
+// function playRound(playerSelection, computerSelection) {
+
+// }
+
+// const playerSelection = "Rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+
+// function game(playRound) {
+//     for (let i=0; i < 5; i++) {
+
+//     }
+// }
