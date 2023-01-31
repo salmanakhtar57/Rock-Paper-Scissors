4c61d6c4a7b0d0f99fc7368608cbb5e6a9a2c80e
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-01-31 16:45:23 +0500

    taking the random number from the comouter

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+
+console.log(getComputerChoice())
