774475a120ef60301a1fce9c062b043c784fef75
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-02-01 12:08:04 +0500

    comple the play round part

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ function getComputerChoice() {
   return choices[randomNumber];
 }
 
-console.log(getComputerChoice())
+// console.log(getComputerChoice())
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-    return "Its a tie"
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You win"
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You win"
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win"
+    return "Its a draw";
+  } else if (playerSelection === "Rock"     && computerSelection === "Scissors" ||
+             playerSelection === "Paper"    && computerSelection === "Rock" ||
+             playerSelection === "scissors" && computerSelection === "paper") {
+    return `You win! ${playerSelection} beats ${computerSelection}`
   } else {
-    return "You lose"
+    return `You lose! ${computerSelection} beats ${playerSelection}`
   }
 }
+
+// console.log(playRound('Paper', 'Rock'))
+
